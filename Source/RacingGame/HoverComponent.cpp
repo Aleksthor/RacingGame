382c470792d593c94f328468e2c8184f9222e606
Aleksthor <athorset98@gmail.com>
2022-03-18 18:03:13 +0100

    Added Player and Movement Inputs
    Now W and S is Forward/Backwards
    Player is hovering and going to move alright.
    Will update movement further.

---
+++ Source/RacingGame/HoverComponent.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HoverComponent.h"
+#include "PlayerCar.h"
+#include "Components/PrimitiveComponent.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UHoverComponent::UHoverComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	TraceLength = 200.f;
+	HoverForce = 50000.f;
+	LinearDamping = LinearDampingDefault;
+	AngularDamping = AngularDampingDefault;
+}
+
+
+// Called when the game starts
+void UHoverComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	AActor* Player = GetOwner();
+	PlayerCar = Cast<APlayerCar>(Player);
+}
+
+
+// Called every frame
+void UHoverComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+
+	if (IsValid(PlayerCar))
+	{
+
+		FVector WorldLocation = GetComponentLocation();
+		FVector UpVector = GetUpVector();
+		UpVector *= TraceLength;
+		FVector EndLocation = WorldLocation - UpVector;
+
+		HitResult;
+		FCollisionQueryParams TraceParams;
+		GetWorld()->LineTraceSingleByChannel(HitResult, WorldLocation, EndLocation, ECC_Visibility, TraceParams);
+
+		if (HitResult.IsValidBlockingHit())
+		{
+			FVector Length = HitResult.Location - WorldLocation;
+			float LengthSize = Length.Size();
+			float Ratio = LengthSize / TraceLength;
+
+			float Lerp = FMath::Lerp(HoverForce, 0.f, Ratio);
+
+			FVector Force = HitResult.ImpactNormal * Lerp;
+
+			Cast<UPrimitiveComponent>(PlayerCar->GetRootComponent())->AddForceAtLocation(Force, WorldLocation);
+		}
+		Cast<UPrimitiveComponent>(PlayerCar->GetRootComponent())->SetLinearDamping(LinearDamping);
+		Cast<UPrimitiveComponent>(PlayerCar->GetRootComponent())->SetAngularDamping(AngularDamping);
+
+	}
+}
+
---
+++ Source/RacingGame/PlayerCar.h
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerCar.generated.h"
+
+class UHoverComponent;
+
+UCLASS()
+class RACINGGAME_API APlayerCar : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerCar();
+
+	/** Component Body */
+	/** Static Mesh */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	class USkeletalMeshComponent* PlayerMesh{ nullptr };
+
+	/** Collision Box */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	class UBoxComponent* Collider{ nullptr };
+
+	/** Spring Arm */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	class USpringArmComponent* SpringArm{ nullptr };
+
+	/** Camera */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	class UCameraComponent* Camera{ nullptr };
+
+	//** MovementComponent */
+	UPROPERTY(VisibleAnywhere)
+	class UPawnMovementComponent* MovementComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	UHoverComponent* HoverComponent1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	UHoverComponent* HoverComponent2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	UHoverComponent* HoverComponent3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+	UHoverComponent* HoverComponent4;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	// Save Pawn Rotation Every Tick
+	FRotator PawnRotation;
+
+	// Save Control Rotation Every Tick
+	FRotator ControlRotation;
+
+
+	UFUNCTION()
+	void Forward(float value);
+
+	UFUNCTION()
+	void Right(float value);
+
+	UFUNCTION()
+	void Pause(float value);
+};
